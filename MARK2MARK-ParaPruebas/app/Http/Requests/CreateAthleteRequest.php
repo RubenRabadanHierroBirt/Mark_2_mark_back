df588a3f7389c4ea5c04969ff151bda7a5b815a5
Martin <mbarrientos@birt.eus>
2026-01-02 18:37:42 +0100

    atletas put estado + delete

--- MARK2MARK-ParaPruebas/app/Http/Requests/CreateAthleteRequest.php
+++ MARK2MARK-ParaPruebas/app/Http/Requests/CreateAthleteRequest.php
@@ -25,6 +25,7 @@ class CreateAthleteRequest extends FormRequest
             'username' => 'sometimes|string|max:50|unique:usuarios,username',
             'usuario' => 'sometimes|string|max:50|unique:usuarios,username',
             'password' => 'sometimes|string|min:6|max:255',
+            'club_nombre' => 'sometimes|string|max:100',
         ];
     }
 }
